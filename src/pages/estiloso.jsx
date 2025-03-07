428547b4491d7df73267a9d92c45592fa941b936
tramalho <thiagofacul@gmail.com>
2025-03-07 06:55:03 -0300

    css modularizado

---
+++ src/pages/estiloso.jsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Estiloso.module.css';
+
+export default function Estiloso() {
+    return (
+        <div className={styles.roxo}>
+            <h1>Estiloso</h1>
+            <h1>Exemplo de um componente com estilo</h1>
+        </div>  
+    );
+}
