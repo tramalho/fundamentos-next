d4205c66246d9c19c4f8eb1de9f83dfbe60cf960
tramalho <thiagofacul@gmail.com>
2025-03-16 07:00:24 -0300

    Compontente Navegador

--- src/pages/exemplo.jsx
+++ src/pages/exemplo.jsx
@@ -1,10 +1,11 @@
 import Cabecalho from "../components/Cabecalho";
+import Layout from "../components/Layout";
 
 export default function Exemplo() {
     return (
-        <>
+        <Layout>
         <Cabecalho titulo="Fundamentos Next.js & React"/>
         <Cabecalho titulo="Personalizando título"/>
-        </>
+        </Layout>
     );
 }
--- src/pages/jsx.jsx
+++ src/pages/jsx.jsx
@@ -1,10 +1,15 @@
+import Layout from "../components/Layout";
+
 export default function Jsx() {
-    const titulo = <h1>Integração JS e JSX</h1>
-    const subtitulo = 'Ao utilizar chaves {} é possível inserir código JS dentro do JSX';
-    return (
-        <div>
-            {titulo}
-            <h2>{subtitulo}</h2>
-        </div>
-    );
+	const titulo = <h1>Integração JS e JSX</h1>;
+	const subtitulo =
+		"Ao utilizar chaves {} é possível inserir código JS dentro do JSX";
+	return (
+		<Layout titulo="Entendendo o JSX">
+			<div>
+				{titulo}
+				<h2>{subtitulo}</h2>
+			</div>{" "}
+		</Layout>
+	);
 }
