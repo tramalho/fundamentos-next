45716d062a154d3f8e7378587aeba813d5a35758
tramalho <thiagofacul@gmail.com>
2025-03-16 07:22:37 -0300

    Navegacao Simples

--- src/components/Navegador.jsx
+++ src/components/Navegador.jsx
@@ -3,10 +3,11 @@ import styles from "../styles/Navegador.module.css";
 
 export default function Navegador(props) {
 	return (
-		<Link href={props.destino}>
-			<div className={styles.navegador}
-            style={{backgroundColor: props.cor || "dodgerblue"}}
-            >{props.texto}</div>
-		</Link>
+		<div>
+			<Link href={props.destino}
+            className={styles.navegador}
+			style={{ backgroundColor: props.cor || "dodgerblue" }}
+            >{props.texto}</Link>
+		</div>
 	);
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,6 +13,7 @@ export default function Home() {
             <Navegador texto="Estiloso" destino="/estiloso" />
             <Navegador texto="Exemplo" destino="/exemplo" cor="darkviolet"/>
             <Navegador texto="JSX" destino="/jsx" cor="crimson"/>
+            <Navegador texto="Navegação #01" destino="/navegacao" cor="green"/>
         </div>
     );
 }
